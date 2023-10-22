7aaf58bd1fc45ca2da819a15ad649812bdbe3375
Oyawe <oyawegoodness@gmail.com>
2023-10-21 18:14:50 -0700

    price category completed

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -2,7 +2,32 @@ import './Price.css'
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className="sidebar-title">Price</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$0 - $50
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$50 - $100
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$100 - $150
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>Over $150
+        </label>
+      </div>
+    </div>
   )
 }
 
