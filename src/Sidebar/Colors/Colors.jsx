195ce976f0dd4517564250c35feeb70ddd97773d
Oyawe <oyawegoodness@gmail.com>
2023-10-21 18:23:19 -0700

    colors done but not dynamic

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,8 +2,37 @@ import './Colors.css';
 
 const Colors = () => {
   return (
-    <div>Colors</div>
-  )
+    <div>
+      <h2 className="sidebar-title">Colors</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>Black
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>Blue
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>Red
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>Green
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>White
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default Colors
